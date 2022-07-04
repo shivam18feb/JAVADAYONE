127ae39621113af8143df959305193a4d35e38a6
unknown <shivam18feb@gmail.com>
2022-07-04 11:43:14 +0530

    CheckPresentAbsentCheck

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,7 +5,18 @@ public static void main(String[] args) {
 			System.out.println(" **********WELCOME TO EMPLOYEE WAGE COMPUTATION*************"); 
 			
 			
+			double empCheck = Math.floor(Math.random() * 10) % 2;
 			
+			if (empCheck == 1){
+			
+				System.out.println("Employee are present");
+				
+			}
+			else{
+					
+					System.out.println("Employee are absent");
+				
+			}
 			
 
 
