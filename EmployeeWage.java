11a9f16915a60028aeaa695aa422c0169c1516b6
unknown <shivam18feb@gmail.com>
2022-07-04 12:10:41 +0530

    usingswitchcases

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,27 +1,27 @@
 public class EmployeeWage{
 
 public static void main(String[] args) {
+			public static final int  IS_PART_TIME = 1;	
+			public static final int  IS_FULL_TIME = 2; 	
+			public static final int  PER_HR = 20; 
+			public static final int  DAY_HR = 8; 
+			public static final int  PART_DAY_HR= 4;
 			
-			int IS_FULL_TIME = 1;
-			int IS_PART_TIME = 2;
 			
-            int PER_HR = 20;
-            int DAY_HR = 8;
-			int PART_TIME_HR = 4;
-            int empWage = 0;
             int empHrs = 0;
 			
 			double empCheck = Math.floor(Math.random() * 10) % 3;
 			
-			if (empCheck == IS_FULL_TIME) {
-                        empHrs = DAY_HR;
-                } 
-				else if (empCheck == IS_PART_TIME) {
-                        empHrs = PART_TIME_HR;
-                }
-				else {
-                        empHrs = 0;
-                }
+			switch ((int) empCheck) {
+ 			case IS_PART_TIME: 
+ 					empHrs = PART_DAY_HR; 
+				break; 
+			case IS_FULL_TIME: 
+ 					empHrs = DAY_HR; 
+ 				break; 
+ 				default: 
+				empHrs = 0; 
+				} 
  
                 empWage = empHrs * PER_HR;
                 System.out.println("Employee Wage: " + empWage);
