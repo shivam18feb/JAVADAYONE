28af5b566d26b5095c6e87d740c4ace42637c012
unknown <shivam18feb@gmail.com>
2022-07-04 12:03:37 +0530

    parttimewage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,16 +3,22 @@ public class EmployeeWage{
 public static void main(String[] args) {
 			
 			int IS_FULL_TIME = 1;
+			int IS_PART_TIME = 2;
+			
             int PER_HR = 20;
             int DAY_HR = 8;
+			int PART_TIME_HR = 4;
             int empWage = 0;
             int empHrs = 0;
 			
-			double empCheck = Math.floor(Math.random() * 10) % 2;
+			double empCheck = Math.floor(Math.random() * 10) % 3;
 			
 			if (empCheck == IS_FULL_TIME) {
                         empHrs = DAY_HR;
                 } 
+				else if (empCheck == IS_PART_TIME) {
+                        empHrs = PART_TIME_HR;
+                }
 				else {
                         empHrs = 0;
                 }
