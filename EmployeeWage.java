26571b4493970c9501a0fbfb6dc05400bd813a8c
unknown <shivam18feb@gmail.com>
2022-07-04 12:21:49 +0530

    wagepermonth

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,30 +1,30 @@
 public class EmployeeWage{
 
-public static void main(String[] args) {
-			public static final int  IS_PART_TIME = 1;	
-			public static final int  IS_FULL_TIME = 2; 	
-			public static final int  PER_HR = 20; 
-			public static final int  DAY_HR = 8; 
-			public static final int  PART_DAY_HR= 4;
-			
-			
-            int empHrs = 0;
-			
-			double empCheck = Math.floor(Math.random() * 10) % 3;
-			
-			switch ((int) empCheck) {
- 			case IS_PART_TIME: 
- 					empHrs = PART_DAY_HR; 
-				break; 
-			case IS_FULL_TIME: 
- 					empHrs = DAY_HR; 
- 				break; 
- 				default: 
-				empHrs = 0; 
-				} 
+public static final int IS_PART_TIME = 1;
+        public static final int IS_FULL_TIME = 2;
+        public static final int PER_HR = 20;
+        public static final int DAY_HR = 8;
+        public static final int PART_DAY_HR = 4;
+        public static final int DAYS_PER_MONTH = 20;
  
-                empWage = empHrs * PER_HR;
-                System.out.println("Employee Wage: " + empWage);
+        public static void main(String[] args) {
+                int days = 1;
+                double salary = 0;
+                while (days <= DAYS_PER_MONTH) {
+                        double empCheck = Math.floor(Math.random() * 10) % 3;
+                        switch ((int) empCheck) {
+                        case IS_PART_TIME:
+                                salary = (salary + (PART_DAY_HR * PER_HR));
+                        break;
+                        case IS_FULL_TIME:
+                                salary = (salary + (DAY_HR * PER_HR));
+                        break;
+                        default:
+                                salary = (salary + 0);
+                        }
+                        days++;
+                }
+                System.out.println("Employee Wage Per Month: " + salary); 
 			
 
 
